77724a27266b27b56be7930656cec2ced1216d54
Сухроб <suhrobarakcaa@gmail.com>
2026-01-30 21:36:59 +0700

    models

--- backend/models/client.py
+++ backend/models/client.py
@@ -1,15 +1,14 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from app.core.database import Base
-
+from database import Base
 
 class Client(Base):
     __tablename__ = "clients"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    full_name: Mapped[str] = mapped_column(String(255), nullable=False)
-    phone: Mapped[str] = mapped_column(String(50))
-    email: Mapped[str] = mapped_column(String(255))
-    address: Mapped[str] = mapped_column(String(255))
+    full_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    phone_number: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    address: Mapped[str] = mapped_column(String(200), nullable=True)
 
-    cars = relationship("Car", back_populates="client")
+
