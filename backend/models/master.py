07d13127573916652bc7324db78b66d410da5856
Сухроб <suhrobarakcaa@gmail.com>
2026-02-02 20:12:01 +0700

    Добавил модели для мастеров

---
+++ backend/models/master.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
+
+
+class Master(Base):
+    __tablename__ = "masters"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    specialization: Mapped[str] = mapped_column(String(255))
