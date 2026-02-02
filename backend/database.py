549150f1a2b83812886f0911632c523d61b7fe3a
Сухроб <suhrobarakcaa@gmail.com>
2026-02-02 20:50:43 +0700

    сделал таблицы и подключился к бд

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres123@localhost:5432/autoservice"
+DATABASE_URL = "postgresql+psycopg2://postgres:postgres123@localhost/automasterskaya_db"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
 from database import Base, engine
+
 from models import client, car, contract, order, service, spare_part, manager, master
-app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Автомастерская API")
+
+
+@app.get("/")
+def root():
+    return {"status": "API работает"}
