53beb148f694cd05a9248ab77a1ba4c6b9d7de02
Сухроб <suhrobarakcaa@gmail.com>
2026-02-03 14:38:48 +0700

    crud-операция для машин

---
+++ backend/crud/car.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from models.car import Car
+from schemas.car import CarCreate, CarUpdate
+
+
+def get_car(db: Session, car_id: int):
+    return db.query(Car).filter(Car.id == car_id).first()
+
+
+def get_cars(db: Session):
+    return db.query(Car).all()
+
+
+def create_car(db: Session, car: CarCreate):
+    db_car = Car(**car.model_dump())
+    db.add(db_car)
+    db.commit()
+    db.refresh(db_car)
+    return db_car
+
+
+def update_car(db: Session, db_car: Car, car: CarUpdate):
+    for field, value in car.model_dump(exclude_unset=True).items():
+        setattr(db_car, field, value)
+
+    db.commit()
+    db.refresh(db_car)
+    return db_car
+
+
+def delete_car(db: Session, db_car: Car):
+    db.delete(db_car)
+    db.commit()
