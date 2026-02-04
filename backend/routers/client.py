cc4ee34681a97925b7db553227c8c75cd76706fa
Сухроб <suhrobarakcaa@gmail.com>
2026-02-04 21:29:13 +0700

    добавил в роутер для обновления и удаления данных

--- backend/routers/client.py
+++ backend/routers/client.py
@@ -13,11 +13,9 @@ from schemas.client import ClientCreate, ClientUpdate
 
 router = APIRouter(prefix="/clients", tags=["Clients"])
 
-
 @router.get("/")
 def read_clients(db: Session = Depends(get_db)):
     return get_clients(db)
-
 
 @router.get("/{client_id}")
 def read_client(client_id: int, db: Session = Depends(get_db)):
@@ -26,9 +24,22 @@ def read_client(client_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Клиент не найден")
     return client
 
-
 @router.post("/")
 def add_client(client: ClientCreate, db: Session = Depends(get_db)):
     return create_client(db, client)
 
+@router.put("/{client_id}")
+def edit_client(client_id: int, client: ClientUpdate, db: Session = Depends(get_db)):
+    db_client = get_client(db, client_id)
+    if not db_client:
+        raise HTTPException(status_code=404, detail="Клиент не найден")
+    return update_client(db, db_client, client)
 
+
+@router.delete("/{client_id}")
+def remove_client(client_id: int, db: Session = Depends(get_db)):
+    db_client = get_client(db, client_id)
+    if not db_client:
+        raise HTTPException(status_code=404, detail="Клиент не найден")
+    delete_client(db, db_client)
+    return {"detail": "Клиент удален"}
